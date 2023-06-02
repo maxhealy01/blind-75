5a62cb2415377b629c35ca29d1121b95899e3802
Max Healy <maxhealy01@gmail.com>
2023-06-02 17:55:32 -0500

    contains dup algo

---
+++ week_1/Array/contains_duplicate.js
@@ -0,0 +1,11 @@
+const containsDuplicate = (nums) => {
+	const map = {};
+	for (i = 0; i < nums.length; i++) {
+		if (map[nums[i]]) {
+			return true;
+		} else {
+			map[nums[i]] = 1;
+		}
+	}
+	return false;
+};
