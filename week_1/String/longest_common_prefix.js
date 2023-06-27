2e0555bcb49be5e60cd03da67981d81d15303ef9
Max Healy <maxhealy01@gmail.com>
2023-06-27 18:30:37 -0500

    longest common prefix algo

---
+++ week_1/String/longest_common_prefix.js
@@ -0,0 +1,14 @@
+const longestCommonPrefix = (arr) => {
+	let longest_common_prefix = "";
+	outer: for (j = 0; j < arr[0].length; j++) {
+		for (i = 1; i < arr.length; i++) {
+			if (arr[i][j] !== arr[0][j]) {
+				break outer;
+			}
+		}
+		longest_common_prefix = arr[0].slice(0, j + 1);
+	}
+	return longest_common_prefix;
+};
+
+module.exports = longestCommonPrefix;
