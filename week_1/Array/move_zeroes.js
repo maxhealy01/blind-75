525a0602f86f2d5588ecce9a3157e21b65b66e9c
Max Healy <maxhealy01@gmail.com>
2023-06-29 18:01:39 -0500

    Added test and description

--- week_1/Array/move_zeroes.js
+++ week_1/Array/move_zeroes.js
@@ -1,3 +1,6 @@
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
 /**
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
