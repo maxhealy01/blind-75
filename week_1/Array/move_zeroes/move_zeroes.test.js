59d9dec254b4966a26045d76ee6c2538061433b7
Max Healy <maxhealy01@gmail.com>
2023-07-05 14:49:10 -0500

    Added tests

--- week_1/Array/move_zeroes/move_zeroes.test.js
+++ week_1/Array/move_zeroes/move_zeroes.test.js
@@ -1,7 +1,21 @@
 const moveZeroes = require("./move_zeroes");
 
-test("move_zeroes moves zeroes to the end of an array of numbers while maintaining the relative order of the remaining elements", () => {
-	let nums = [0, 10, 0, 2, 3, 4, 0, 5, 6, 7];
-	moveZeroes(nums);
-	expect(nums).toEqual([10, 2, 3, 4, 5, 6, 7, 0, 0, 0]);
+describe("move_zeroes", () => {
+	test("moves zeroes to the end of an array of numbers while maintaining the relative order of the remaining elements", () => {
+		let nums = [0, 10, 0, 2, 3, 4, 0, 5, 6, 7];
+		moveZeroes(nums);
+		expect(nums).toEqual([10, 2, 3, 4, 5, 6, 7, 0, 0, 0]);
+	});
+
+	test("changes nothing when there are no zeroes", () => {
+		let nums = [1, 2, 3, 4, 5];
+		moveZeroes(nums);
+		expect(nums).toEqual([1, 2, 3, 4, 5]);
+	});
+
+	test("changes nothing when zeroes are already on the end", () => {
+		let nums = [1, 2, 3, 4, 5, 0, 0, 0];
+		moveZeroes(nums);
+		expect(nums).toEqual([1, 2, 3, 4, 5, 0, 0, 0]);
+	});
 });
