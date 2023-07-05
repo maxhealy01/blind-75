bdbe52455e36b1bd4a0ea67fac28f34dea9f3e63
Max Healy <maxhealy01@gmail.com>
2023-07-05 16:08:28 -0500

    First submission of sort colors algorithm

---
+++ week_1/Array/sort_colors/sort_colors.js
@@ -0,0 +1,36 @@
+// Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+// We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+// Warning: This algorithm mutates the input array.
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const sortColors = (nums) => {
+	let low = 0;
+	let current = 0;
+	let high = nums.length - 1;
+
+	while (current <= high) {
+		// This swap ensures that every value whose index < low is equal to 0
+		if (nums[current] == 0) {
+			swap(low, current);
+			low++;
+			current++;
+		}
+		// This swap ensures that every value whose index > high is equal to 2
+		else if (nums[current] == 2) {
+			swap(current, high);
+			high--;
+		} else {
+			current++;
+		}
+	}
+
+	function swap(a, b) {
+		[nums[a], nums[b]] = [nums[b], nums[a]];
+	}
+};
+
+module.exports = sortColors;
---
+++ week_1/Array/sort_colors/sort_colors.test.js
@@ -0,0 +1,33 @@
+const sortColors = require("./sort_colors");
+
+describe("sort_colors", () => {
+	test("modifies array in place to sort colors", () => {
+		let nums = [1, 2, 0, 1, 2, 2, 1, 0];
+		sortColors(nums);
+		expect(nums).toEqual([0, 0, 1, 1, 1, 2, 2, 2]);
+	});
+
+	test("modifies backwards array", () => {
+		let nums = [2, 1, 0];
+		sortColors(nums);
+		expect(nums).toEqual([0, 1, 2]);
+	});
+
+	test("returns same array if only 0's", () => {
+		let nums = [0, 0, 0, 0, 0];
+		sortColors(nums);
+		expect(nums).toEqual([0, 0, 0, 0, 0]);
+	});
+
+	test("returns same array if only 1's", () => {
+		let nums = [1, 1, 1, 1, 1];
+		sortColors(nums);
+		expect(nums).toEqual([1, 1, 1, 1, 1]);
+	});
+
+	test("returns same array if only 2's", () => {
+		let nums = [2, 2, 2, 2, 2];
+		sortColors(nums);
+		expect(nums).toEqual([2, 2, 2, 2, 2]);
+	});
+});
