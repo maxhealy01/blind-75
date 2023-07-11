9deb2ef5a14961a17b510e990f570a50c6e21a45
Max Healy <maxhealy01@gmail.com>
2023-07-11 13:39:51 -0500

    Added JSDoc and explanation to ransom_note.js

--- Hash_Table/ransom_note/ransom_note.js
+++ Hash_Table/ransom_note/ransom_note.js
@@ -1,3 +1,9 @@
+// Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
 const canConstruct = (ransomNote, magazine) => {
 	if (magazine.length < ransomNote.length) return false;
 
